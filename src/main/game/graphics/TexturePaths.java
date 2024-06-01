5c27a1906b96cd4daf7fc88e0b3466abf7ce4189
GabrielPw <gxaavier352@gmail.com>
2024-06-01 13:21:28 -0300

    [funcional] carregamento de textura por tile

---
+++ src/main/game/graphics/TexturePaths.java
@@ -0,0 +1,10 @@
+public final class TexturePaths {
+
+    private TexturePaths(){}
+
+    public static final String textureAtlas1  = "src/main/resources/assets/atlas/atlas1.png";
+    public static final String textureGrass  = "src/main/resources/assets/grassblock.jpg";
+    public static final String textureWooden = "src/main/resources/assets/wooden2.jpg";
+    public static final String textureTileBlue  = "src/main/resources/assets/tile32/tileblue.png";
+    public static final String textureTileGreen = "src/main/resources/assets/tile32/tilegreen.png";
+}
